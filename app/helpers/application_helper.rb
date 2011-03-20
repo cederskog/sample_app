43b423a506a40cc25978e34f605112fab524526f
Cederskog <christopher@cederskog.de>
2011-03-20 18:21:16 +1100

    Done with sign in and sign out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,7 @@ module ApplicationHelper
   end
 
   def logo
-    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
-    
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
 
 
